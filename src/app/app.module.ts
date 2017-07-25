55acf4cc8c0df0747ee746b59c536936dfa48d36
lzjslqq <chief.king@qq.com>
2017-07-25 15:48:56 +0800

    添加引导组件介绍

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { LoginComponent } from './login/login.component';
   ],
   // providers列出会在此模块中“注入”的服务（Service）
   providers: [],
+  // bootstrap指明哪个组件为引导性组件（本案例中的AppComponent）。
+  // 当Angular引导应用时，它会在DOM中渲染这个引导性组件，并把结果放进index.html的该组件的元素标签中（本案例中的app-root）
   bootstrap: [AppComponent]
 })
 export class AppModule { }
